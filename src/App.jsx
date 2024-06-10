01f09da2f2e4cf89992a1e8fba2f97586809d166
anubhavseet <150212867+anubhavseet@users.noreply.github.com>
2024-06-10 12:31:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"))
   const [room, setRoom] = useState(null)
   const roomInputRef = useRef(null)
-  const passwordRef = useRef(null)
+  const passwordInputRef = useRef(null)
 
   const handleEnterChat = () => {
     const roomName = roomInputRef.current.value.trim()
@@ -45,41 +45,47 @@ function App() {
           </div>
         ) : (
             <div className="bg-white shadow-md rounded-lg px-8 py-6 mb-4 flex flex-col items-center">
-            <div className="mb-4">
-              <label className="block text-gray-700  text-xl font-bold mb-2" htmlFor="room-name">
-                Enter room name to chat .
-              </label>
-              <input
-                type="text"
-                ref={roomInputRef}
-                defaultValue=""
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="room-name"
-                placeholder="Enter Room Name"
-              />
-              <input
-                type="text"
-                ref={passwordref}
-                defaultValue=""
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                placeholder="Enter password"
-              />
-            </div>
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-              onClick={handleEnterChat}
-            >
-              Enter Chat
-            </button>
-            <button
-              className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mt-4 focus:outline-none focus:shadow-outline"
-              onClick={handleSignOut}
-            >
-              Sign Out
-            </button>
-          </div>
+  <div className="mb-4">
+    <label className="block text-gray-700 text-xl font-bold mb-2" htmlFor="room-name">
+      Enter room name to chat.
+    </label>
+    <input
+      type="text"
+      ref={roomInputRef}
+      defaultValue=""
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      id="room-name"
+      placeholder="Enter Room Name"
+    />
+  </div>
+  <div className="mb-4">
+    <label className="block text-gray-700 text-xl font-bold mb-2" htmlFor="password">
+      Enter password.
+    </label>
+    <input
+      type="password"
+      ref={passwordInputRef}
+      defaultValue=""
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      id="password"
+      placeholder="Enter Password"
+    />
+  </div>
+  <button
+    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+    type="button"
+    onClick={handleEnterChat}
+  >
+    Enter Chat
+  </button>
+  <button
+    className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mt-4 focus:outline-none focus:shadow-outline"
+    onClick={handleSignOut}
+  >
+    Sign Out
+  </button>
+</div>
+
 
         
 
